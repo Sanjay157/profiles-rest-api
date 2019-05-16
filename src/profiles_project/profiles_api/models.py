a9be0b7b423ddce8830667d11fe5b5a4a61b204d
S <sanjay.s3@dxc.com>
2019-05-16 12:25:09 +0530

    Added custom user profile model, a manager and migration for our database

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,8 +1,37 @@
 from django.db import models                                #default
 from django.contrib.auth.models import AbstractBaseUser     #imported
-from django.contrib.auth.models import PermissionsMixin     #imported
-
+from django.contrib.auth.models import PermissionsMixin     #imported #Allows to give permission as to what and what not to do for user model
+from django.contrib.auth.models import BaseUserManager      #imported to work with Object Manager/UserProfileManager
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    #Helps Django to work with our custom user model.
+
+    def create_user(self, email, name, password=None):              #STANDARD USER
+        #Create a new user profile object.
+
+        if not email:                                               #if the email field is blank then an error will be raised
+            raise ValueError("User must have a vaild email ID")
+
+        email = self.normalize_email(email)                         #normalize will convert all the characters to lowercase letters
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+         
+
+        return user 
+
+    def create_superuser(self, email, name, password):              #SUPER USER
+        #Creates and saves a new superuser with the given details.
+
+        user = self.create_user(email, name, password)  
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using = self._db)
+        return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     #Represents a user profile inside our system.
@@ -13,7 +42,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     #ObjectManager is a class used to help manage the profile giving 
-    # additional functionality like creating a admin user or creting a regular user
+    # additional functionality like creating a admin user or creating a regular user
 
     objects =  UserProfileManager()
 

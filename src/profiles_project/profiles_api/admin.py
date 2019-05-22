dc75e79eb793c6d2b9a970dcb70be26bbe4949ab
S <sanjay.s3@dxc.com>
2019-05-22 11:18:47 +0530

    Added model for storing profile feed items, created migration and added to django admin.

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -3,3 +3,4 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.UserProfile)                          #To see in admin page we need to register here 
+admin.site.register(models.ProfileFeedItem)
